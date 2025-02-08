d3b0594dbd18c5a0c10fb7999667f455d322302b
Lived404 <mihranhovhannes12@gmail.com>
2025-02-08 17:45:22 +0400

    part 1

---
+++ constans/cotalog.js
@@ -0,0 +1,74 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Gibson Les Paul Standard 60s AAA Lemon Burst гитара эксклюзив #221940277',
+        img: 'img/Catalog/gitar.jpg',
+        price: 1702000
+    },
+    {
+        id: 'el2',
+        name: 'Gibson Custom Shop 1958 Les Paul Standard Reissue VOS Washed Cherry #841327',
+        img: 'img/Catalog/gitar2.jpg',
+        price: 1200000
+    },
+    {
+        id: 'el3',
+        name: 'Ibanez SR305E 5 струн Deep Ocean Metallic',
+        img: 'img/Catalog/gitar3.jpg',
+        price: 207000
+    },
+    {
+        id: 'el4',
+        name: 'Gibson Les Paul Standard 60s AAA Lemon Burst гитара эксклюзив #221940277',
+        img: 'img/Catalog/gitar.jpg',
+        price: 1702000
+    },
+    {
+        id: 'el5',
+        name: 'Gibson Custom Shop 1958 Les Paul Standard Reissue VOS Washed Cherry #841327',
+        img: 'img/Catalog/gitar2.jpg',
+        price: 1200000
+    },
+    {
+        id: 'el6',
+        name: 'Ibanez SR305E 5 струн Deep Ocean Metallic',
+        img: 'img/Catalog/gitar3.jpg',
+        price: 207000
+    },
+    {
+        id: 'el7',
+        name: 'Gibson Les Paul Standard 60s AAA Lemon Burst гитара эксклюзив #221940277',
+        img: 'img/Catalog/gitar.jpg',
+        price: 1702000
+    },
+    {
+        id: 'el8',
+        name: 'Gibson Custom Shop 1958 Les Paul Standard Reissue VOS Washed Cherry #841327',
+        img: 'img/Catalog/gitar2.jpg',
+        price: 1200000
+    },
+    {
+        id: 'el9',
+        name: 'Ibanez SR305E 5 струн Deep Ocean Metallic',
+        img: 'img/Catalog/gitar3.jpg',
+        price: 207000
+    },
+    {
+        id: 'el10',
+        name: 'Gibson Les Paul Standard 60s AAA Lemon Burst гитара эксклюзив #221940277',
+        img: 'img/Catalog/gitar.jpg',
+        price: 1702000
+    },
+    {
+        id: 'el11',
+        name: 'Gibson Custom Shop 1958 Les Paul Standard Reissue VOS Washed Cherry #841327',
+        img: 'img/Catalog/gitar2.jpg',
+        price: 1200000
+    },
+    {
+        id: 'el12',
+        name: 'Ibanez SR305E 5 струн Deep Ocean Metallic',
+        img: 'img/Catalog/gitar3.jpg',
+        price: 207000
+    },
+]
